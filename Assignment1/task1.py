b9420474d862ec9e05c31e0a1ea472ca152c0856
GauravMandli <gauravmandli2004@gmail.com>
2025-07-27 14:58:49 +0530

    commit

---
+++ Assignment1/task1.py
@@ -0,0 +1,12 @@
+num1=float(input("Enter First Number:  "))
+num2=float(input("Enter Second Number:  "))
+
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+
+print("The sum is:",add)
+print("The Subtraction is:",sub)
+print("The Multiplication is:",mul)
+print("The Division is:",div)
