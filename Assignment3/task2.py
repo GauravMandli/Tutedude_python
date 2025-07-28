03949de4cdc518cfe5f04700f3678f78fe5133d6
GauravMandli <gauravmandli2004@gmail.com>
2025-07-28 19:21:42 +0530

    commit

---
+++ Assignment3/task2.py
@@ -0,0 +1,15 @@
+import math
+
+num = float(input("Enter Number: "))  # Convert input to float
+
+square = math.pow(num, 2)
+root = math.sqrt(num)
+log = math.log(num)
+#sr = math.radians(num)
+sin = math.sin(num)
+
+print("Your Number is:", num)
+print("Your num's Square is:", square)
+print("Your num's Square Root is:", root)
+print("Your num's Log is:", log)
+print("Your num's Sin is:", sin)
